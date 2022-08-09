d94671baac9b6d3eaf47a9a2d2dc1c4f9bf26cd6
Salmane <88793063+salmaane@users.noreply.github.com>
2022-08-09 20:48:34 +0100

    portfolio

---
+++ script.js
@@ -0,0 +1,7 @@
+const menu = document.getElementById("menu")
+const menuicon = document.getElementById("menu-icon")
+
+function openMenu(){
+    menu.classList.toggle("is-active");
+    menuicon.classList.toggle("is-active")
+}
